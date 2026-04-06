0043170afb28877a9b1ba2d391cf5df72139686d
eungsoo <os9722@gmail.com>
2026-04-06 19:34:41 +0900

    feat/ 실시간 날씨 작업

---
+++ src/main/java/com/es/ta/live/WeatherDTO.java
@@ -0,0 +1,67 @@
+package com.es.ta.live;
+
+public class WeatherDTO {
+    private String cityName;
+    private String description;
+    private double temp;
+    private double feelsLike;
+    private int humidity;
+    private double windSpeed;
+    private String iconUrl;
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getTemp() {
+        return temp;
+    }
+
+    public void setTemp(double temp) {
+        this.temp = temp;
+    }
+
+    public double getFeelsLike() {
+        return feelsLike;
+    }
+
+    public void setFeelsLike(double feelsLike) {
+        this.feelsLike = feelsLike;
+    }
+
+    public int getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(int humidity) {
+        this.humidity = humidity;
+    }
+
+    public double getWindSpeed() {
+        return windSpeed;
+    }
+
+    public void setWindSpeed(double windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+
+    public String getIconUrl() {
+        return iconUrl;
+    }
+
+    public void setIconUrl(String iconUrl) {
+        this.iconUrl = iconUrl;
+    }
+}
